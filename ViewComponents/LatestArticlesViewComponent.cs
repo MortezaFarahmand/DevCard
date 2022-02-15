26a1ccc998c1477b1724e07e4458769ffca0a7d3
MortezaFarahmand <farahmand.morteza@gmail.com>
2022-02-16 00:15:35 +0330

    Article Added

---
+++ ViewComponents/LatestArticlesViewComponent.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.Collections.Generic;
+using DevCar_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCar_MVC.ViewComponents
+{
+    public class LatestArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>
+            {
+                new Article(1 , "دوره مدیریت مهندسی تعمیر و نگهداری هواپیما" , "این دوره مخصوص معاونت های مهندسی و تعمیرات هر سازمان می باشد که البته تمامی علاقه مندان می توانند شرکت کنند." ,
+                    "blog-post-thumb-1.jpg"),
+                new Article(2 , "مقاله بهینه سازی تعمیر و نگهداری هواپیما" , "این دوره مخصوص معاونت های مهندسی و تعمیرات هر سازمان می باشد که البته تمامی علاقه مندان می توانند شرکت کنند." ,
+                    "blog-post-thumb-2.jpg"),
+                new Article(3 , "مقاله کنترل و تولید آشیانه تعمیرات" , "این دوره مخصوص معاونت های مهندسی و تعمیرات هر سازمان می باشد که البته تمامی علاقه مندان می توانند شرکت کنند." ,
+                    "blog-post-thumb-3.jpg"),
+                new Article(4 , "مقاله بهسازی فرایند های سرویس دهی " +
+                                "ایستگاههای تعمیرات" , "این دوره مخصوص معاونت های مهندسی و تعمیرات هر سازمان می باشد که البته تمامی علاقه مندان می توانند شرکت کنند." ,
+                    "blog-post-thumb-4.jpg"),
+                new Article(5 , "مقاله مانیتورینگ موتورهای توربین گازی" , "این دوره مخصوص معاونت های مهندسی و تعمیرات هر سازمان می باشد که البته تمامی علاقه مندان می توانند شرکت کنند." ,
+                    "blog-post-thumb-5.jpg")
+            };
+            return View("_LatestArticles" , articles);
+        }
+    }
+}
---
+++ ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Collections.Generic;
+using DevCar_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCar_MVC.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>
+            {
+                new Project(1,"انبارداری" ,"سیستم درخواست و کنترل و پیشبینی قطعات انبارها" , "project-1.jpg" , "TEMM_1" ) ,
+                new Project(2,"کنترل و تولید" ,"سیستم کنترل و تولید آشیانه تعمیرات و مهندسی و خط پرواز ها" , "project-2.jpg" , "TEMM_2" ) ,
+                new Project(3,"تداوم صلاحیت پروازی" ,"سیستم تداوم صلاحیت پروازی پارت ام" , "project-3.jpg" ,"TEMM_3" ) ,
+                new Project(4,"پرسنل" ,"سیستم کنترل پرسنل و کنترل ورود و خروج و چیدمان" , "project-4.jpg" , "TEMM_4" ) ,
+            };
+            return View("_Projects", projects);
+        }
+    }
+}
